8e7df9d06ef61321798291f72c687129b486b2f0
Diego <diego.tanel@tecneg.com>
2012-06-04 12:40:43 -0300

    Commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Prefectura::Application.config.secret_token = '10499448738c0e10d80486c66c8e595eb405406f31fb2d83d350fa7fe9ab947e033325923da1d8d94fa98db3652abad5db4b6c1ca49377b721df6783899668bb'
