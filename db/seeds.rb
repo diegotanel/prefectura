8590d439c565fa06cb517da45a9454d621ad6d81
Diego Tanel <diego.tanel@tecneg.com>
2013-07-16 19:47:22 -0300

    corrección del nombre ws_helper.rb, se agrego .rvmrc y el seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Urlws.create!(:url => "http://200.41.238.203:8899/reports.asmx?WSDL")
+
+@origenes = [
+  ["Hidrovia_PZDE", "COZA"],
+  ["Hidrovia_PZRP", "COBA"],
+  ["Hidrovia_PZBP", "CORO"],
+  ["Hidrovia_General", "Todos"]
+]
+
+@origenes.each { |ori|
+  OrigenDeReporte.create!(:codigo => ori[0], :etiqueta => ori[1])
+}
